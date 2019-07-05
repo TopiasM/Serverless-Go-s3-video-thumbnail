cc23e0abf5d8a3562eadd0f1b9264309f16b58a3
TopiasM <ubuntu@ip-172-31-22-17.ap-northeast-2.compute.internal>
2019-07-05 21:37:09 +0900

    Added file extension configuration. Cleaned serverless.yml

--- main/main.go
+++ main/main.go
@@ -20,9 +20,12 @@ import (
 )
 
 func Handler(ctx context.Context, s3Event events.S3Event) {
-	sess, _ := session.NewSession(&aws.Config{
+	sess, err := session.NewSession(&aws.Config{
 		Region: aws.String(os.Getenv("AWS_REGION")),
 	})
+	if err != nil {
+		log.Print(err)
+	}
 
 	svc := s3.New(sess)
 
@@ -32,10 +35,14 @@ func Handler(ctx context.Context, s3Event events.S3Event) {
 		Bucket: aws.String(os.Getenv("S3_BUCKET")),
 		Key: aws.String(vidKey),
 	})
+	if err != nil {
+		log.Print(err)
+	}
 
-	log.Printf("%s to thumb", vidKey)
+	log.Printf("%s to thumbnail", vidKey)
 
-	out, err := os.Create("/tmp/vid.mp4")
+	tmpVidPath := strings.Join([]string("/tmp/video", os.GetEnv("VIDEO_EXTENSION")))
+	out, err := os.Create(tmpVidPath)
 	defer out.Close()
 
 	resp := result
@@ -44,18 +51,18 @@ func Handler(ctx context.Context, s3Event events.S3Event) {
 	_, err = io.Copy(out, resp.Body)
 
 	path, _ := filepath.Abs("./bin/ffmpeg");
-	cmd := exec.Command(path, "-i", "/tmp/vid.mp4", "-movflags", "faststart" ,"-ss", os.Getenv("SCREENSHOT_TIME"), "-vframes", "1", "-s", os.Getenv("RESOLUTION"), "/tmp/thumb.jpg")
+	tmpThumbPath := strings.Join([]string{"/tmp/thumb", os.Getenv("THUMB_EXTENSION")}, "")
+	cmd := exec.Command(path, "-i", tmpVidPath, "-movflags", "faststart" ,"-ss", os.Getenv("SCREENSHOT_TIME"), "-vframes", "1", "-s", os.Getenv("RESOLUTION"), tmpThumbPath)
 	err = cmd.Run()
 	if err != nil {
 		log.Print(err)
 	}
-	defer os.Remove("/tmp/thumb.jpg")
+	defer os.Remove(tmpThumbPath)
 
 	nameSlice := strings.Split(vidKey, "/")
 	typeSlice := strings.Split(nameSlice[1], ".")
-	s := []string{nameSlice[0], "-thumb/", typeSlice[0], ".jpg"}
-	thumbKey := strings.Join(s, "")
-	dat, _ := ioutil.ReadFile("/tmp/thumb.jpg")
+	thumbKey := strings.Join([]string{nameSlice[0], "-thumb/", typeSlice[0], os.Getenv("THUMB_EXTENSION")}, "")
+	dat, _ := ioutil.ReadFile(tmpThumbPath)
 
 	uploader := s3manager.NewUploader(sess)
 	_, err = uploader.Upload(&s3manager.UploadInput{
